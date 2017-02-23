2229281d4a33b9b0121685dff39e12de4f9042a0
Sharon Umute <Sharon Umute>
2017-02-22 22:52:44 -0800

    Testing React code

--- src/components/ImageBrowser/ImageBrowser.js
+++ src/components/ImageBrowser/ImageBrowser.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
+var ScrollArea = require('react-scrollbar');
+
 export default class ImageBrowser extends Component {
 
   render() {
 
     return (
-      <div>
-        Image Browser
-      </div>
+      <ScrollArea
+            speed={0.8}
+            className="area"
+            contentClassName="content"
+            horizontal={false}
+            >
+          <div>
+            Image Browser
+          </div>
+      </ScrollArea>
     );
   }
 }
