b65bf0bd4a76d84df38f14fd335ddd2b037f43fc
Sharon <sharongirl21@gmail.com>
2017-03-06 11:11:38 -0800

    The image browser takes in url links from the form on the top of the page and displays the images in grids and at a fixed size. I just have to figure out how to add menus to each one

--- src/components/ImageBrowser/ImageBrowser.js
+++ src/components/ImageBrowser/ImageBrowser.js
@@ -4,10 +4,118 @@ export default class ImageBrowser extends Component {
 
   render() {
 
-    return (
-          <div>
-            Image Browser Stuff
+    var body ={
+      padding: 50,
+      backgroundColor: "#E9E6E6",
+      fontFamily: "sans-serif"
+    }
+    var listMainHeaderInput ={
+      padding: 10,
+      fontSize: 16,
+      border: 2,
+      solid: "#FFF"
+    }
+    var listMainHeaderButton ={
+      padding: 10,
+      fontSize: 16,
+      margin: 10,
+      backgroundColor: "#0066FF",
+      color: "#FFF",
+      border: 2, 
+      solid: "#0066FF"
+    }
+    
+    var todoListMainTheListLi= {
+      backgroundColor: "#E9E6E6",
+      padding: 30,
+      marginBottom: 30,
+      width: 300,
+      height: 300
+    }
+
+    var destination = document.querySelector("#container");
+
+    var TodoItems = React.createClass({
+      render: function() {
+        var todoEntries = this.props.entries;
+
+        function createTasks(item) {
+          return <img key={item.key} alt={item.key} style={todoListMainTheListLi} src={item.text} />
+        }
+    
+        var listItems = todoEntries.map(createTasks);
+
+        return (
+          <ul className="theList">
+            {listItems.reverse()}
+          </ul>
+        );
+      }
+    });
+
+    var TodoList = React.createClass({
+
+      getInitialState: function() {
+        return {
+          items: [] //accessed via this.state.items
+        };
+      },
+
+      addItem: function(e) {
+        var itemArray = this.state.items;
+  
+        itemArray.push(
+          {
+            text: this._inputElement.value,
+            key: Date.now()
+          }
+        );
+      
+        this.setState({
+          items: itemArray
+        });
+      
+        this._inputElement.value = "";
+      
+        e.preventDefault();
+      },
+
+      render: function() {
+        return (
+          <div className="todoListMain">
+            <div className="header">
+              <form onSubmit={this.addItem}>
+                <input ref={(a) => this._inputElement = a} placeholder="enter image url" style={listMainHeaderInput}>
+                </input>
+                <button type="submit" style={listMainHeaderButton}>add</button>
+              </form>
+            </div>
+            <TodoItems entries={this.state.items}/>
           </div>
+        );
+      }
+    });
+
+
+    return (
+      <html>
+      <head>
+        <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
+        <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>        
+      </head>
+
+      <body style={body}>
+        <div id="container">
+          <TodoList/>
+        </div>
+        {destination}
+
+        <script type="text/babel">
+        </script>
+      </body>
+      </html>
+              
     );
   }
 }
