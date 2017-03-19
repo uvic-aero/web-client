fda4314136800a8ae8baf2941a42cab46a09c173
Sharon <drewcarter101@hotmail.com>
2017-03-18 22:37:10 -0700

    Tidying up Image browser code

--- src/components/ImageBrowser/ImageBrowser.js
+++ src/components/ImageBrowser/ImageBrowser.js
@@ -17,108 +17,15 @@ function mapStateToProps(state, props) {
 
 class ImageBrowser extends Component {
 
-  render() {
-
-    var body ={
-      padding: 50,
-      backgroundColor: "#E9E6E6",
-      fontFamily: "sans-serif"
-    }
-    var listMainHeaderInput ={
-      padding: 10,
-      fontSize: 16,
-      border: 2,
-      solid: "#FFF"
-    }
-    var listMainHeaderButton ={
-      padding: 10,
-      fontSize: 16,
-      margin: 10,
-      backgroundColor: "#0066FF",
-      color: "#FFF",
-      border: 2, 
-      solid: "#0066FF"
-    }
-    
-    var todoListMainTheListLi= {
-      backgroundColor: "#E9E6E6",
-      padding: 30,
-      marginBottom: 30,
-      width: 300,
-      height: 300
-    }
-
-    var destination = document.querySelector("#container");
-
-    var TodoItems = React.createClass({
-      render: function() {
-        var todoEntries = this.props.entries;
-
-        function createTasks(item) {
-          return
-          {this.props.images.map((img, i) => (
-              <img key={i} alt={i} style={todoListMainTheListLi} src={this.props.images[i].url} />
-            ))}
-          
-        }
-    
-        var listItems = todoEntries.map(createTasks);
-
-        return (
-          <ul className="theList">
-            {listItems.reverse()}
-          </ul>
-        );
-      }
-    });
-
-    var TodoList = React.createClass({
-
-      getInitialState: function() {
-        return {
-          items: [] //accessed via this.state.items
-        };
-      },
-
-      addItem: function(e) {
-        var itemArray = this.state.items;
-  
-        itemArray.push(
-          {
-            text: this._inputElement.value,
-            key: Date.now()
-          }
-        );
-      
-        this.setState({
-          items: itemArray
-        });
-      
-        this._inputElement.value = "";
-      
-        e.preventDefault();
-      },
-
-      render: function() {
-        return (
-          <div className="todoListMain">
-            <div className="header">
-              
-            </div>
-          </div>
-        );
-      }
-    });
-
+  render() {  
 
     return (
-      <div style={body}>
+      <div style={MainPage}>
         <div id="container">
           {this.props.images.map((img, i) => (
-              <img key={i} alt={i} style={todoListMainTheListLi} src={this.props.images[i].url} />
-            ))}
+              <img key={i} alt={i} style={ImageGrid} src={this.props.images[i].url} />
+            )).reverse()}
         </div>
-        {destination}
 	  </div>
     );
   }
