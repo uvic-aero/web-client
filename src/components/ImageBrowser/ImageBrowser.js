351d5a6dc3ec1c4c170ff325cb932409c651d21b
Sharon <drewcarter101@hotmail.com>
2017-03-18 23:14:30 -0700

    Cleaned up image browser

--- src/components/ImageBrowser/ImageBrowser.js
+++ src/components/ImageBrowser/ImageBrowser.js
@@ -7,8 +7,6 @@ import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import cx from 'classnames';
 import s from './ImageBrowser.css';
-import Image from '../ImageQueue/Image';
-import ImageDock from '../ImageQueue/ImageDock';
 
 function mapStateToProps(state, props) {
   return {
@@ -22,10 +20,10 @@ class ImageBrowser extends Component {
   render() {  
 
     return (
-      <div>
+      <div className={s.mainPage}>
         <div>
           {this.props.images.map((img, i) => (
-              <img key={i} alt={i} style={s.imageGrid} src={this.props.images[i].url} />
+              <img key={i} alt={i} className={s.imageGrid} src={this.props.images[i].url} />
             )).reverse()}
         </div>
 	  </div>
