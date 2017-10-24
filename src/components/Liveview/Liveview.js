f4dc61df1d004eb37927c4e9aaff1bf8be9d24f0
Unknown <chrishampu@outlook.com>
2017-10-24 11:32:44 -0700

    Add basic liveview image container and capture button

--- src/components/Liveview/Liveview.js
+++ src/components/Liveview/Liveview.js
@@ -3,13 +3,31 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import s from "./Liveview.css";
 
+import RaisedButton from "material-ui/RaisedButton";
+
 function mapStateToProps(state, props) {
   return {};
 }
 
+const LiveviewUrl = "http://127.0.0.1:8080/liveview.jpg";
+
 class Liveview extends Component {
+  captureStill() {}
+
   render() {
-    return <div className={s.root} />;
+    return (
+      <div className={s.root}>
+        <div className={s.image_container}>
+          <img role="presentation" src={LiveviewUrl} className={s.image} />
+          <RaisedButton
+            onTouchTap={() => this.captureStill()}
+            className={s.capture_button}
+            label="Capture Still"
+            labelColor="black"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
