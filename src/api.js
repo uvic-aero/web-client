b4414fbe6670499938af10602d756464d812429f
Unknown <chrishampu@outlook.com>
2018-04-23 18:07:30 -0700

    Connect liveview to groundstation api

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import "whatwg-fetch";
+
+// API url on the GroundStation
+const Endpoint = "http://127.0.0.1:24002";
+
+function post(url, body = {}, options = {}) {
+  const payload = typeof body === "string" ? body : JSON.stringify(body);
+
+  return fetch(`${Endpoint}${url}`, {
+    method: "POST",
+    body: payload,
+    headers: {
+      "content-type": "application/json"
+    },
+    ...options
+  }).then(res => res.json());
+}
+
+function get(url, options = {}) {
+  return fetch(`${Endpoint}${url}`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json"
+    },
+    ...options
+  }).then(res => res.json());
+}
+
+// Tell the onboard computer to capture a still image during liveview playback
+export function captureStill() {
+    return get('/camera/still');
+}
+
+export function zoomIn() {
+  return get('/camera/zoomin');
+}
+
+export function zoomOut() {
+  return get('/camera/zoomout');
+}
+
--- src/components/Liveview/Liveview.js
+++ src/components/Liveview/Liveview.js
@@ -3,28 +3,43 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import s from "./Liveview.css";
 
+import { captureStill, zoomIn, zoomOut } from "../../api";
+
 import RaisedButton from "material-ui/RaisedButton";
 
 function mapStateToProps(state, props) {
   return {};
 }
 
-const LiveviewUrl = "http://127.0.0.1:8080/liveview.jpg";
+const liveviewUrl = "http://127.0.0.1:8000/liveview.jpg";
 
 class Liveview extends Component {
-  captureStill() {}
 
   render() {
     return (
       <div className={s.root}>
         <div className={s.image_container}>
-          <img role="presentation" src={LiveviewUrl} className={s.image} />
-          <RaisedButton
-            onTouchTap={() => this.captureStill()}
-            className={s.capture_button}
-            label="Capture Still"
-            labelColor="black"
-          />
+          <img role="presentation" src={liveviewUrl} className={s.image} />
+          <div className={s.buttons}>
+            <RaisedButton
+              onTouchTap={() => zoomOut()}
+              className={s.capture_button}
+              label="Zoom out"
+              labelColor="black"
+            />
+            <RaisedButton
+              onTouchTap={() => zoomIn()}
+              className={s.capture_button}
+              label="Zoom in"
+              labelColor="black"
+            />
+            <RaisedButton
+              onTouchTap={() => captureStill()}
+              className={s.capture_button}
+              label="Capture Still"
+              labelColor="black"
+            />
+          </div>
         </div>
       </div>
     );
