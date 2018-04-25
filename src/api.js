c8fd846c8fa58467275aa51b4d08c1b72021bcd2
Chris Hampu <chrishampu@outlook.com>
2018-04-24 21:09:56 -0700

    Add camera status tracking to liveview

--- src/api.js
+++ src/api.js
@@ -28,14 +28,21 @@ function get(url, options = {}) {
 
 // Tell the onboard computer to capture a still image during liveview playback
 export function captureStill() {
-    return get('/camera/still');
+  return get("/camera/still");
 }
 
 export function zoomIn() {
-  return get('/camera/zoomin');
+  return get("/camera/zoomin");
 }
 
 export function zoomOut() {
-  return get('/camera/zoomout');
+  return get("/camera/zoomout");
 }
 
+export function getMode() {
+  return get("/camera/mode");
+}
+
+export function setMode(mode) {
+  return post("/camera/mode", { wanted: mode });
+}
--- src/components/Liveview/Liveview.js
+++ src/components/Liveview/Liveview.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import s from "./Liveview.css";
 
-import { captureStill, zoomIn, zoomOut } from "../../api";
+import { captureStill, zoomIn, zoomOut, getMode, setMode } from "../../api";
 
 import RaisedButton from "material-ui/RaisedButton";
 
@@ -11,16 +11,86 @@ function mapStateToProps(state, props) {
   return {};
 }
 
-const liveviewUrl = "http://127.0.0.1:8000/liveview.jpg";
+const liveviewUrl = "http://127.0.0.1:5000";
+
+const mode2str = mode =>
+  mode === 0 ? "None" : mode === 1 ? "Still" : mode === 2 ? "Live" : "Unknown";
 
 class Liveview extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      statusTimer: null,
+      statusCall: false,
+      currentMode: mode2str(-1),
+      wantedMode: mode2str(-1)
+    };
+  }
+
+  statusHandler() {
+    if (this.state.statusCall) {
+      return;
+    }
+
+    getMode()
+      .then(modes =>
+        this.setState({
+          statusCall: false,
+          currentMode: mode2str(modes.current),
+          wantedMode: mode2str(modes.wanted)
+        })
+      )
+      .catch(() =>
+        this.setState({
+          currentMode: mode2str(-1),
+          wantedMode: mode2str(-1),
+          statusCall: false
+        })
+      );
+
+    this.setState(() => ({
+      statusCall: true
+    }));
+  }
+
+  componentDidMount() {
+    const statusTimer = setInterval(this.statusHandler.bind(this), 2000);
+    this.setState(() => ({ statusTimer }));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.statusTimer);
+  }
 
   render() {
     return (
       <div className={s.root}>
+        <div className={s.status_container}>
+          <div className={s.status_block}>
+            <div className={s.status_header}>Current Mode</div>
+            <div className={s.status_content}>{this.state.currentMode}</div>
+          </div>
+          <div className={s.status_block}>
+            <div className={s.status_header}>Target Mode</div>
+            <div className={s.status_content}>{this.state.wantedMode}</div>
+          </div>
+        </div>
         <div className={s.image_container}>
           <img role="presentation" src={liveviewUrl} className={s.image} />
           <div className={s.buttons}>
+            <RaisedButton
+              onTouchTap={() => setMode(1)}
+              className={s.capture_button}
+              label="Set Still Mode"
+              labelColor="black"
+            />
+            <RaisedButton
+              onTouchTap={() => setMode(2)}
+              className={s.capture_button}
+              label="Set Live Mode"
+              labelColor="black"
+            />
             <RaisedButton
               onTouchTap={() => zoomOut()}
               className={s.capture_button}
