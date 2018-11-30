d27575178025247e733a9f82b92f72afbccb6ce7
Dragon Prevost <dragon@dkp.io>
2018-11-30 15:29:18 -0800

    created placeholder class for services interface

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,21 @@
+import react, { Component } from 'react';
+
+
+class Services extends Component{
+	state = {
+    
+	}
+
+
+	render(){
+		return(
+			<div className='container'>
+				<div className='row'>
+					<p>This is where services go</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Services
