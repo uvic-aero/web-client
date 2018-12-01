cda0a390ca52020b9f211ed4507d062cfea80bca
Dragon Prevost <dragon@dkp.io>
2018-11-30 18:04:15 -0800

    adjusted titles for each component

--- src/components/Services/VideoDisplay.jsx
+++ src/components/Services/VideoDisplay.jsx
@@ -5,11 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class VideoDisplay extends Component{
 	state = {
+		status:'Disconnected'
 	}
 	render(){
 		return(
-			<div className='col-sm'>
-				<p>Video Display</p>
+			<div className='col-sm m-3 border'>
+				<p className='h1'>Video Display</p>
+				<span className='h4 fluid'>Status: {this.state.status}</span>
+				<button type='button' onClick={this.handleStart} className='btn btn-success btn-block m-2'> Start </button> 
+				<button type='button' onClick={this.handleStart} className='btn btn-danger btn-block m-2'> Stop </button>
+				<button type='button' onClick={this.handleStart} className='btn btn-primary btn-block m-2'> Reload Status </button> 
 			</div>
 		);
 	}
