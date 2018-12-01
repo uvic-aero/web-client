36ccb03749bd0f18989ac02cc9b6f1c417d01823
Dragon Prevost <dragon@dkp.io>
2018-11-30 16:56:14 -0800

    added buttons to imgservice comp

--- src/components/Services/ImageService.jsx
+++ src/components/Services/ImageService.jsx
@@ -5,11 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class ImageService extends Component{
 	state = {
+		status: 'broken'
 	}
+	
+	handleStart(){
+		console.log('starting ImageService');
+	}
+	
 	render(){
 		return(
-			<div className='col-sm'>
-				<p>Image Service</p>
+			<div className='col-sm m-3 border'>
+				<p className='h1'>Image Service</p>
+				<span className='h4 fluid'>Status: {this.state.status}</span>
+				<button type='button' onClick={this.handleStart} className='btn btn-success btn-block m-2'> Start </button> 
+				<button type='button' onClick={this.handleStart} className='btn btn-danger btn-block m-2'> Stop </button>
+				<button type='button' onClick={this.handleStart} className='btn btn-primary btn-block m-2'> Reload Status </button> 
 			</div>
 		);
 	}
