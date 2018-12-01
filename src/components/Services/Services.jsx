bce988a9d4f2553b703502fee2d30cadb7484e59
Dragon Prevost <dragon@dkp.io>
2018-11-30 16:18:30 -0800

    added all three components to services comp

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,17 +1,22 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ImageService from './ImageService';
+import Timelapse from './Timelapse';
+import VideoDisplay from './VideoDisplay';
 
 class Services extends Component{
 	state = {
-    
 	}
-
-
 	render(){
 		return(
 			<div className='container'>
 				<div className='row'>
-					<p>This is where services go</p>
+					<ImageService />
+					<VideoDisplay />
+					<Timelapse />
 				</div>
 			</div>
 		);
